2e90b1b0da84400ba8764794908e30faeec13f47
teawolfy <teresa.elizabeth.wolf@gmail.com>
2016-09-02 12:22:33 -0400

    Exercises for Homework 1

--- calc.py
+++ calc.py
@@ -1,4 +1,11 @@
-print('Q1:',42*60+60)
-print('Q2:',10/1.61)
+#VARIABLES
+seconds = 42*60+60      #convert to seconds
+miles = 10/1.61         #convert to miles
+spm = seconds/miles     #calculates seconds per miles
+apm = spm/60            #calculates pace in minutes
 
+#QUESTIONS
+print('Q1:',seconds)
+print('Q2:',miles)
+print('Q3: average pace=',apm,'mph=', 60/apm)
 
