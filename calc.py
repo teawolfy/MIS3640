0b39dd56864efeadd8089629430149e87def5572
teawolfy <teresa.elizabeth.wolf@gmail.com>
2016-09-08 13:24:12 -0400

    pull request accepted

--- calc.py
+++ calc.py
@@ -24,7 +24,7 @@ shipping = 3+((quantity-1)*.75)     #shipping cost
 easy_tempo= 2*8.25                  #time at easy tempo
 hard_tempo= 3*7.2                   #time at hard tempo
 total_time= easy_tempo + hard_tempo #total time spent running
-end_time = total time - 8           #the amount of minutes past 7:00am
+end_time = total_time - 8           #the amount of minutes past 7:00am
 #Question 4
 percent = ((89-82)/82)*100          #calculates total percentage
 
