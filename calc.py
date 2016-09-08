ff947388011b79ff18fe79e47c9a4b4c7a4425e7
teawolfy <teresa.elizabeth.wolf@gmail.com>
2016-09-07 21:21:21 -0400

    Homework Two

--- calc.py
+++ calc.py
@@ -12,14 +12,29 @@ print('Q3: average pace=',apm,'mph=', 60/apm)
 
 print('EXERCISES CLASS TWO')
 #VARIABLES
+#Question One
 pi = 3.14159                        #value of pi
 r = 5                               #value of radius
+#Question Two
 price = 24.95                       #price of book
 discount = .4*price                 #wholesale discount
 quantity = 60                       #number of books
 shipping = 3+((quantity-1)*.75)     #shipping cost
+#Question 3
+easy tempo= 2*8.25                  #time at easy tempo
+hard tempo= 3*7.2                   #time at hard tempo
+total time= easy tempo + hard tempo #total time spent running
+end time = total time - 8           #the amount of minutes past 7:00am
+#Question 4
+percent = ((89-82)/82)*100          #calculates total percentage
+
 
 #QUESTIONS
 print('Q1: volume=', (4/3)*pi*r**3)
 print('Q2: wholesale cost=', (price-discount)*quantity+shipping)
+print('Q3: 7:%d' % (end time))
+print('Q4: %.1f%%' % percent)
+
+
+
 
