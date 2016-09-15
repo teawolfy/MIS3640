946374343d681f4b65f407507b29f8a118e38e0d
teawolfy <teresa.elizabeth.wolf@gmail.com>
2016-09-15 13:59:28 -0400

    correct solution for Quadratic Funciton

--- Classwork/class_four/function_e1.py
+++ Classwork/class_four/function_e1.py
@@ -15,3 +15,12 @@ else:
     x1 = (-b + math.sqrt(b**2 - 4*a*c))/2*a
     x2 = (-b + math.sqrt(b**2 - 4*a*c))/2*a        
     print("This equation has two solutions: "), x1, print(" and"), x2
+
+#correct solution
+def quadratic(a, b, c):
+    try:
+        x1 = (-b + (sqrt(b**2 - 4*a*c))) / (2*a)
+        x2 = (-b - (sqrt(b**2 - 4*a*c))) / (2*a)
+        return x1, x2
+    except ValueError:
+        print('These values will not work')
