7d77369c41581be927f4f3644b208cb8d5dfdea8
teawolfy <teresa.elizabeth.wolf@gmail.com>
2016-09-14 22:14:03 -0400

    Homework, and In-Class Exercises

---
+++ Exercises/function_e1.py
@@ -0,0 +1,17 @@
+import math
+
+a = float(input('Enter a: '))
+b = float(input('Enter b: '))
+c = float(input('Enter c: '))
+
+d = b**2-4*a*c #descriminant 
+
+if d < 0:
+    print("This equation has no real solution")
+elif d == 0:
+    x = (-b + math.sqrt(b**2 - 4*a*c))/2*a
+    print("This equation has one solution: "), x
+else:
+    x1 = (-b + math.sqrt(b**2 - 4*a*c))/2*a
+    x2 = (-b + math.sqrt(b**2 - 4*a*c))/2*a        
+    print("This equation has two solutions: "), x1, print(" and"), x2
--- In-Class Examples/class_four/classwork.py
+++ In-Class Examples/class_four/classwork.py
@@ -9,3 +9,30 @@ my_abs(100)
 my_abs(0)
 
 my_abs(-4)
+
+def give_me_a_break():
+    str1 = 'break'
+    return str1
+
+print(give_me_a_break())
+
+def nop():
+    pass
+
+age= int(input())
+if age >= 18:
+    pass
+
+import math
+def move(x, y, step, angle):
+    nx = x + step * math.cos(angle)
+    ny = y - step * math.sin(angle)
+    return nx, ny
+
+x, y = move(100, 100, 60, math.pi / 6)
+print(x, y)
+
+#Homework for tonight
+def quadratic(a, b, c):
+
+    pass
